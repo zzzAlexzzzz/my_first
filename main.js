77a9825bbd7763ee17a1a273ece3cbc5c5227371
zzzAlexzzzz <hudorozhkov1985@mail.ru>
2023-04-11 16:19:41 +0500

    2 урок

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
 console.log('Hello ' + 'world!' + 66);// строка всегда в кавычках
 console.log(666);
 console.log(2+2);
+console.log(9);
+console.log('Добро '+'пожаловать'+'на курс'+' Алексей');
+// alert('09.04.23');
+// let a = Infinity
+// console.log(1/0);
+
+// console.log('stroka'/2);
+
+// console.log(99999999999999999999999n);
+
+// let str1='JOJO'
+// let str2="Jojo"
+// let str3=`ANIME${str2}`
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+
+// console.log(false==0);
+
+// let nuller=null
+// console.log(nuller);
+
+// let a;
+// console.log(a);
+
+typeof
+typeof x 
+typeof(x)
+console.log(typeof'cat');
+console.log(typeof 2);
+console.log(typeof true);
+console.log(typeof 10n);
+console.log(typeof undefined);
+console.log(typeof null);
