5c257993f762c4d28f8794fd6245ac3cbbe014e3
zzzAlexzzzz <hudorozhkov1985@mail.ru>
2023-04-18 17:50:45 +0500

    анятие 4. Операторы

--- main.js
+++ main.js
@@ -39,5 +39,136 @@
 // console.log(typeof undefined);
 // console.log(typeof null);
 
-document.querySelector('#one').innerHTML = 666
+// document.querySelector('#one').innerHTML = 666
+// document.querySelector('p').innerHTML = 'wsefuw9efg7'
 
+// let inp = document.querySelector('.text')
+// let btn = document.querySelector('.check')
+// let out = document.querySelector('.out')
+
+
+// btn.onclick = function() {
+//     console.log('МЫ что-то вывели');
+//     let e = inp.value
+//     out.innerHTML = e
+//     inp.value = ''
+// }
+
+// let myhappy = prompt('Когда у тебя др?')
+
+
+// if(myhappy >= 100){
+//     document.querySelector('.out').innerHTML = 'Yes'
+// }else{
+//     console.log(myhappy);
+// 
+
+
+// let year = prompt('В каком году вышел ходячий замок?')
+// if(year <2004){
+// console.log('Рано');
+// }
+// else if(year>2004){
+// console.log('Поздно');
+// }
+// else{
+// console.log('Верно');
+// }
+
+// // не совсем верная запись
+// if(year <2004){
+// console.log('Рано');
+// }
+// if(year>2004){
+// console.log('Поздно');
+// }
+// if(year ==2004){
+// console.log('Верно');
+// }
+
+
+// let day = prompt('Сколько днй в неделе?')
+// let week = (day == 7) ? 'Отлично' : 'Ты не умеешь считать'
+// let Результат = Условие ? если True Первый вариант : иначе Второй вариант
+// if(day== 7){
+// week =true
+// }
+// else{
+// week = false
+// }
+// console.log(week);
+
+ //< > - оператор больше или меньше
+//>= <= - Больше или равно меньше или равно
+//== - Не строгое равно
+// === - CТрогое равно
+// = - оператор присваивания
+// != - не равно
+
+// ||
+// true || true = true
+// false || true = true
+// true || false = true
+// false || false = false
+
+// &&
+// true && true = true
+// false && true = false
+// true && false = false
+// false && false = false
+
+// let a = 4
+// let b =10
+// if(a > 5 || a > 5){
+//     console.log(b);
+// }
+
+
+
+let sr = prompt("больше 15")
+
+if(sr > 15){
+    console.log('да');
+}
+else if(sr < 15){
+    console.log('нет');
+}
+else{
+    console.log('равно');
+}
+
+// let a = 10
+// let b = 5
+
+// if (a + b > 15) {
+//     console.log("да");
+// }
+
+// else if (a + b == 15){
+//     console.log("нет");
+// }
+// else {
+//     console.log("нет");
+// }
+
+// let a = 2;
+// let b = 2;
+// if (a === b) {
+//   console.log("равны");
+// } else {
+//   console.log("не равны");
+// }
+
+// let result = (a === b);
+// console.log(result);
+
+// let c = 'xxx';
+// let d = 2;
+// if (c === d) {
+//   console.log("равны");
+// } else {
+//   console.log("не равны");
+// }
+
+// let result = (c === d);
+// console.log(result);
